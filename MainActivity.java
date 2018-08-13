3af36264e779fabb5357912940480f44f6dde511
alex4oya <42258083+alex4oya@users.noreply.github.com>
2018-08-12 21:30:14 -0500

    hola
    Signed-off-by: alex4oya <42258083+alex4oya@users.noreply.github.com>

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package desarrollador_alejandro_manilla.cachfox.pruebagit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
